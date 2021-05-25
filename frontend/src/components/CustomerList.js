565b86a1f1a04ca1c12cc9b6cf56db81495ec857
joaquin <gamboanj@gmail.com>
2021-05-24 20:15:43 -0400

    delete user

--- frontend/src/components/CustomerList.js
+++ frontend/src/components/CustomerList.js
@@ -11,13 +11,18 @@ class CustomerList extends Component {
         this.setState({customers:result.data})
     }
 
+    async delete(cid) {
+        let result = await axios.delete(`http://localhost:8000/customers/${cid}/`);
+        alert(`Customer id ${cid} deleted`)
+    }
+
     render() {
         return (
             <div className="customer-container">
                 <div>
                     {this.state.customers.map(customer => (
                         <ul className="customer-list">
-                            <h1>{customer.name} Company {customer.company} Priority  {customer.priority}</h1>
+                            <h1>{customer.name} {customer.company} {customer.priority}</h1><button className="delete"onClick={() => this.delete(customer.id)}>Delete</button>
                         </ul>
                      ))}
                 </div>
